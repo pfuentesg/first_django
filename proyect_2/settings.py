fd0ebaf71d4119f28009d46d1ec4ae3ce0ec41bf
pfuentesg <pablofuentesg1@yahoo.es>
2018-03-30 12:54:37 +0200

    static files ok

--- proyect_2/settings.py
+++ proyect_2/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_DIR = os.path.join(BASE_DIR, 'proyect_2/templates')
-print(TEMP_DIR)
+STATIC_DIR = os.path.join(BASE_DIR,'proyect_2/static')
+print(STATIC_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
